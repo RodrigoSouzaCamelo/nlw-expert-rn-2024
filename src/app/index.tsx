ebbb143a11f4d09305bcc5049965cafaa0b1bf81
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-02-06 21:08:54 -0300

    Criação do projeto e adição do nativetailwind

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="flex-1 pt-8">
+      <Text className="bg-white">Hello world teste</Text>
+    </View>
+  );
+}
