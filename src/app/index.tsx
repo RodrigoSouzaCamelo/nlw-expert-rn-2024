2ac94f8261072c0608e285ef27cba8f1af2880b8
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-02-07 19:55:01 -0300

    Configuração de fonte e criação do componente de loading

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Text className="bg-white">Hello world teste</Text>
+      <Text className="text-white text-2xl font-heading">React Native!</Text>
     </View>
   );
 }
