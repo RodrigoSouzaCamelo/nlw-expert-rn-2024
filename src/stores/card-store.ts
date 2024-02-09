3cf0275c3fe84abbee630a27c54dcb294f5cb507
Rodrigo Souza Camelo <rodrigo.souza.camelo@gmail.com>
2024-02-09 19:43:51 -0300

    Adição de funcionalidade para salvar dados do carrinho mesmo se app for fechado

--- src/stores/card-store.ts
+++ src/stores/card-store.ts
@@ -1,6 +1,8 @@
 import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
 import * as cartInMemory from './helpers/card-in-memory';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export type ProductCartProps = ProductProps & {
   quantity: number;
@@ -12,15 +14,24 @@ type StateProps = {
   remove: (productId: string) => void;
 }
 
-export const useCardStore = create<StateProps>((set) => ({
-  products: [],
-  add: (product: ProductProps) => {
-    set((state) => ({
-      products: cartInMemory.add(state.products, product)
-    }))
-  },
-  remove: (productId: string) => 
-    set((state) => ({
-      products: cartInMemory.remove(state.products, productId)
-    }))
-}));
+export const useCardStore = create(
+  persist<StateProps>((set) => ({
+    products: [],
+
+    add: (product: ProductProps) => {
+      set((state) => ({
+        products: cartInMemory.add(state.products, product)
+      }))
+    },
+
+    remove: (productId: string) =>
+      set((state) => ({
+        products: cartInMemory.remove(state.products, productId)
+      }))
+    }),
+    {
+      name: 'nlw-expert:cart',
+      storage: createJSONStorage(() => AsyncStorage)
+    }
+  )
+);
